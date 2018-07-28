fbf99375d4e7bdd17c9a687738918bafd1df7674
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2018-07-27 19:09:34 -0600

    json fix

--- Mountain Spoke/scripts/wayfinding.js
+++ Mountain Spoke/scripts/wayfinding.js
@@ -1,16 +1,16 @@
 var urlString = document.location.href;
 var urlArray = urlString.split('/');
-var pageHREF = urlArray[urlArray.length-1];
+var pageHREF = urlArray[urlArray.length - 1];
 
 if (pageHREF !== "") {
     var menu = document.querySelectorAll('ul#mainNav li a');
     var i;
 
-    for(var i=0; i<menu.length; i++){
+    for (var i = 0; i < menu.length; i++) {
         var currentURL = (menu[i].getAttribute("href"));
-        menu[i].parentNode.className="";
+        menu[i].parentNode.className = "";
         if (currentURL === pageHREF) {
-            menu[i].parentNode.className="active";
-        } 
+            menu[i].parentNode.className = "active";
+        }
     }
 }
